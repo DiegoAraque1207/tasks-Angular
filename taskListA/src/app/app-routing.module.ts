c441a54148ecb7785507e6e6ca90cda99a53a8a8
Diego <daaraquem@unal.edu.co>
2019-12-17 15:52:14 -0500

    changes

--- taskListA/src/app/app-routing.module.ts
+++ taskListA/src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'task',
+    loadChildren: () => import('./task/task.module').then( m => m.TaskPageModule)
+  },
+  { path: 'task/:id', loadChildren: () => import('./task/task.module').then( m => m.TaskPageModule)},
 ];
 
 @NgModule({
